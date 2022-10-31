dea9659b3f2c780399a0c830f84993bcdd0d3b94
Mahesh <55107148+maheshn0205@users.noreply.github.com>
2022-10-31 15:06:14 -0700

    Create hangman.py
    10/31/2022 hangman game initial commit.

---
+++ hangman.py
@@ -0,0 +1,108 @@
+# hangman game
+import random
+from re import L
+print('''  
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/      
+''')
+
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+lives = 6
+
+display = []
+show_display = ""
+for _ in range(word_length):
+    display += "_"
+    
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+        if guess not in chosen_word:
+            print(f"The {guess} letter is not in the word.")
+            print(stages[lives])
+            lives -= 1
+            if lives < 0:
+                end_of_game = True
+                print("You lost the game")
+            break
+          #print(stages[lives])
+          
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
